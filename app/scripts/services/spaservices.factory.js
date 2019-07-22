7aeca6d337d965fc068bf1fae4e30ee4a0b1ae95
francisco.jimenez <francisco.jimenez@gmail.com>
2019-07-22 08:51:39 -0500

    Se agrega la versión inicial de spa feliz

--- app/scripts/services/spaservices.factory.js
+++ app/scripts/services/spaservices.factory.js
@@ -15,9 +15,9 @@ function spaServices() {
  
   var dataServices = [
     { id:'1', name:'Masaje sencillo', cost:'60000', description:'30 min ', endHappy:false, image:'https://pur.vamtam.com/wp-content/uploads/2017/12/pic6.jpg'},
-    { id:'2', name:'Masaje completo', cost:'90000', description:'60 min ', endHappy:false, image:'https://pur.vamtam.com/wp-content/uploads/2017/12/pic6.jpg'},
+    { id:'2', name:'Masaje completo', cost:'90000', description:'60 min ', endHappy:false, image:'https://pur.vamtam.com/wp-content/uploads/2017/12/pic7.jpg'},
     { id:'3', name:'Masaje full', cost:'120000', description:'90 min ', endHappy:false, image:'https://pur.vamtam.com/wp-content/uploads/2017/12/pic6.jpg'},
-    { id:'4', name:'Masaje chocoloterapia', cost:'150000', description:'100 min ', endHappy:false, image:'https://pur.vamtam.com/wp-content/uploads/2017/12/pic6.jpg'},
+    { id:'4', name:'Masaje chocoloterapia', cost:'150000', description:'100 min ', endHappy:false, image:'https://pur.vamtam.com/wp-content/uploads/2017/12/pic7.jpg'},
     { id:'5', name:'Masaje mascarilla', cost:'30000', description:'30 min ', endHappy:false, image:'https://pur.vamtam.com/wp-content/uploads/2017/12/pic6.jpg'}
   ];
 
